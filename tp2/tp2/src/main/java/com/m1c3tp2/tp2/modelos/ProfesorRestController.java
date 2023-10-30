9a1fd98fea37a94e3cd83556d1e9bd8fb2caddc0
juandejunin <juandejunin75@gmail.com>
2023-10-30 07:21:01 +0100

    feat: init project

---
+++ tp2/tp2/src/main/java/com/m1c3tp2/tp2/modelos/ProfesorRestController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m1c3tp2.tp2.modelos;
+
+/**
+ *
+ * @author A
+ */
+public class ProfesorRestController {
+    
+}
